b67011b89873838e40edbb46db39d2214792d390
Sylvain Ung <sylvainung@live.com>
2018-01-29 11:46:42 -0800

    Serves static files

--- server/staticfileserver.js
+++ server/staticfileserver.js
@@ -1,7 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
-
-const server = express();
+const parser = require('body-parser');
+const morgan = require('morgan');
 const PORT = 3000;
 
-server.listen(PORT, () => console.log('Serving static files on port ', PORT));
+const server = express();
+
+server.use(parser.urlencoded({ extended: true }));
+server.use(parser.json());
+server.use(morgan('tiny'));
+
+server.use(express.static(path.resolve(__dirname, '../client/public')));
+
+server.listen(PORT, () => {
+  console.log('Serving static files on port', PORT);
+});
