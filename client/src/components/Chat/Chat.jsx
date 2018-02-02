a5e461f98a1941f2464a67a793061f349968b108
David Darley <daviddar232@gmail.com>
2018-02-01 19:19:57 -0800

    merge

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -3,10 +3,25 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
 import Login from '../Auth/Login.jsx';
 import { Route, Switch } from 'react-router-dom';
+import io from 'socket.io-client/dist/socket.io.js';
 
 class Chat extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      socket: null
+    }
+  }
+  componentWillMount() {
+    const socket = io('http://localhost:4155', {
+      query: {
+        roomId: location.pathname.slice(1)
+      }
+    })
+    this.setState({
+      socket: this.socket
+    })
+    console.log(this.state);
   }
   render() {
     return (
