5b25d5a7688af277902889e3f7e579c12235fbfa
Sylvain Ung <sylvainung@live.com>
2018-02-02 10:36:24 -0800

    Renders signup component with conditionals by user type

--- client/src/components/Auth/Signup.jsx
+++ client/src/components/Auth/Signup.jsx
@@ -6,19 +6,79 @@ import { withRouter } from 'react-router';
 class Signup extends Component {
   constructor(props) {
     super(props);
+    
+    this.state = {
+      userType: ''
+    }
+
+    this.selectHandler = this.selectHandler.bind(this);
+  }
+
+  selectHandler(e) {
+    this.setState({
+      userType: e.target.value
+    });
   }
 
   render() {
     return (
       <div>
         Hello from signup!
+        {/* Standard form displayed on page load */}
         <form action="/api/auth/signup" method="post">
-          <input/>
-          <input/>
-          <input/>
-          <input/>
-          <input/>
+          <div>
+            Select User Type:
+            <select 
+              name="type" 
+              defaultValue="select"
+              onChange={this.selectHandler}
+            >
+              <option name="select" value="">Select User Type</option>
+              <option name="tenant" value="tenant">Tenant</option>
+              <option name="manager" value="manager">Manager</option>
+            </select>
+          </div>
+          
+          <div>
+            Full Name:
+            <input name="full-name" placeholder="Enter Full Name"/>
+          </div>
+
+          <div>
+            Email:
+            <input name="email" placeholder="Enter Email"/>
+          </div>
+
+          <div>
+            Username:
+            <input name="username" placeholder="Enter Username"/>
+          </div>
+
+          <div>
+            Phone:
+            <input name="phone" placeholder="Phone Number"/>
+          </div>
+
+          <div>
+            Password:
+            <input name="password" placeholder="Enter Password"/>
+          </div>
         </form>
+
+        {/* Conditional properties form based on selected user type */}
+        {
+          !this.state.userType ? null :
+          this.state.userType === 'tenant' ? 
+            <div>
+              Property (Tenant):
+            </div>
+          :
+          this.state.userType === 'manager' ? 
+            <div>
+              Property (Manager):
+            </div>
+          : null
+        }  
       </div>
     );
   }
@@ -26,7 +86,7 @@ class Signup extends Component {
 
 const mapStateToProps = state => {
   return {
-    //
+    userData: state.userData
   }
 };
 
