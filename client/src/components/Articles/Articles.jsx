85a57a2c358ec4c3eb467a099de7ba95dc072ddd
Jaehyun Chi <jaehchi@gmail.com>
2018-02-05 10:20:02 -0800

    [add] - grabbing article data from store

--- client/src/components/Articles/Articles.jsx
+++ client/src/components/Articles/Articles.jsx
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { setArticlesData } from '../../actions/setArticlesData'
+import { setArticlesData } from '../../actions/setArticlesData';
 import axios from 'axios';
-
-// import actions from actions;
 
 class Articles extends Component {
   constructor (props) {
     super(props)
   }
-  componentWillMount () {
-    const D = this.props.articleData ? axios.get('http://localhost:3396/api/articles/fetchAllArticles/1') : {}
-    D.data ? (this.props.setArticlesData(d.data), this.props.history.push('/viewArticles')) : console.log('error bitchass')
+
+  async componentWillMount () {
+    const articles = await axios.get('http://localhost:3396/api/articles/fetchAllArticles/1');
+    articles ? this.props.setArticlesData(articles.data) : console.log('no articles');
+
   }
+
   render  () {
 
     return (
       <div>
-        {console.log('userdata', this.props.userData)}
-        {this.props.userData ? <div>{JSON.stringify(this.props.userData)}</div> : <div>NO USER DATA IN REDUX</div>}
+        {this.props.articlesData ? <div>{JSON.stringify(this.props.articlesData)}</div> : <div>NO article DATA IN REDUX</div>}
         HELLO FROM ARTICLES
       </div>
     )
@@ -29,13 +29,14 @@ class Articles extends Component {
 const mapStateToProps = state => { 
   return {
     userData: state.userData,
-    articleData: state.articlesData
+    articlesData: state.articlesData
   }
 };
 
 const matchDispatchToProps = dispatch => {
+  console.log('dispatch,', dispatch)
   return bindActionCreators({
-    setArticlesData,
+    setArticlesData: setArticlesData // get a post mosterajsdkfl
   }, dispatch);
 };
 
