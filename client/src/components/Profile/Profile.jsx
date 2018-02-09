6eb20c96f85731aa8d053c3e1e7c1b7115262751
Sylvain Ung <sylvainung@live.com>
2018-02-08 17:20:32 -0800

    Adds ability for user to edit user data

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -8,32 +8,19 @@ import axios from 'axios';
 class Profile extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      full_name: localStorage.getItem('full_name'),
+      email: localStorage.getItem('email'),
+      username: localStorage.getItem('username'),
+      phonenumber: localStorage.getItem('phonenumber')
+    };
+
+    this.inputChangeHandler = this.inputChangeHandler.bind(this);
   }
 
   componentDidMount() {
     this.props.setProfileEditState(0);
-  }
-
-  componentDidUpdate() {
-    (this.props.profileEditState === 1) ?
-      (document.getElementsByName('email')[0].value = localStorage.getItem('email') ? 
-        localStorage.getItem('email')
-        : 
-        null,
-      document.getElementsByName('full_name')[0].value = localStorage.getItem('full_name') ? 
-        localStorage.getItem('full_name')
-        : 
-        null,
-      document.getElementsByName('username')[0].value = localStorage.getItem('username') ? 
-        localStorage.getItem('username') 
-        : 
-        null,
-      document.getElementsByName('phonenumber')[0].value = localStorage.getItem('phonenumber') ? 
-        localStorage.getItem('phonenumber')
-        : 
-        null) 
-      : 
-      null;
   }
 
   async onEditHandler() {
@@ -41,67 +28,89 @@ class Profile extends Component {
   }
 
   async onUpdateHandler() {
-    // sylvain pls stuff me
     console.log('stuff me sylvain');
     // handle updating passwords via bcrypt
-    // add any additional fields i.e. properties, secret key (managers), etc.
+    
+    const updateBody = {
+      user_id: localStorage.getItem('id'),
+      full_name: this.state.full_name,
+      email: this.state.email,
+      username: this.state.username,
+      phonenumber: this.state.phonenumber
+    }
+
+    await axios.put('http://localhost:3396/api/users/edit', updateBody);
+
+    localStorage.setItem('username', this.state.username);
+    localStorage.setItem('full_name', this.state.full_name);
+    localStorage.setItem('email', this.state.email);
+    localStorage.setItem('phonenumber', this.state.phonenumber);
+
+    await this.props.setProfileEditState(0);
   }
 
   async onCancelHandler() {
     await this.props.setProfileEditState(0);
   }
 
+  inputChangeHandler(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   render() {
     return (
       <div>
         <h3>PROFILE</h3><br/>
-        {localStorage.getItem('type') === '0' ?
-          <div>
-            <h5>MEMBERS ONLY</h5>
-            <br/>
-            {this.props.profileEditState === 0 ? 
-              <div>
-                FULL NAME:<div>{localStorage.getItem('full_name')}</div><br/>
-                EMAIL:<div>{localStorage.getItem('email')}</div><br/>
-                USERNAME:<div>{localStorage.getItem('username')}</div><br/>
-                PHONE NUMBER:<div>{localStorage.getItem('phonenumber')}</div><br/>
-                <button onClick={this.onEditHandler.bind(this)}>EDIT</button></div> 
-              : 
-              <div>
-                FULL NAME: <input type='text' name='full_name'></input><br/>
-                EMAIL: <input type='text' name='email'></input><br/>
-                USERNAME: <input type='text' name='username'></input><br/>
-                PASSWORD: <input type='text' name='password' defaultValue='replaceme'></input><br/>
-                PHONE NUMBER: <input type='text' name='phonenumber'></input><br/>
-                <button onClick={this.onUpdateHandler.bind(this)}>UPDATE</button>
-                <button onClick={this.onCancelHandler.bind(this)}>CANCEL</button>
-              </div>
-            }
-          </div>
-          :
-          <div>
-            <h5>OWNERS ONLY</h5>
-            <br/>
-            {this.props.profileEditState === 0 ? 
-              <div>
-                FULL NAME:<div>{localStorage.getItem('full_name')}</div><br/>
-                EMAIL:<div>{localStorage.getItem('email')}</div><br/>
-                USERNAME:<div>{localStorage.getItem('username')}</div><br/>
-                PHONE NUMBER:<div>{localStorage.getItem('phonenumber')}</div><br/>
-                <button onClick={this.onEditHandler.bind(this)}>EDIT</button></div> 
-              : 
-              <div>
-                FULL NAME: <input type='text' name='full_name'></input><br/>
-                EMAIL: <input type='text' name='email'></input><br/>
-                USERNAME: <input type='text' name='username'></input><br/>
-                PASSWORD: <input type='password' name='password' defaultValue='replaceme'></input><br/>
-                PHONE NUMBER: <input type='text' name='phonenumber'></input><br/>
-                <button onClick={this.onUpdateHandler.bind(this)}>UPDATE</button>
-                <button onClick={this.onCancelHandler.bind(this)}>CANCEL</button>
-              </div>
-            }
-          </div>
-        }
+        <div>
+          <br/>
+          {this.props.profileEditState === 0 ? 
+            <div>
+              FULL NAME:<div>{localStorage.getItem('full_name')}</div><br/>
+              EMAIL:<div>{localStorage.getItem('email')}</div><br/>
+              USERNAME:<div>{localStorage.getItem('username')}</div><br/>
+              PHONE NUMBER:<div>{localStorage.getItem('phonenumber')}</div><br/>
+              <button onClick={this.onEditHandler.bind(this)}>EDIT</button></div> 
+            : 
+            <div>
+              FULL NAME: 
+              <input 
+                type='text' 
+                name='full_name' 
+                onChange={this.inputChangeHandler} 
+                defaultValue={localStorage.getItem('full_name')}
+              /><br/>
+
+              EMAIL: 
+              <input 
+                type='text' 
+                name='email' 
+                onChange={this.inputChangeHandler} 
+                defaultValue={localStorage.getItem('email')}
+              /><br/>
+
+              USERNAME: 
+              <input 
+                type='text' 
+                name='username' 
+                onChange={this.inputChangeHandler} 
+                defaultValue={localStorage.getItem('username')}
+              /><br/>
+
+              PHONE NUMBER: 
+              <input 
+                type='text' 
+                name='phonenumber' 
+                onChange={this.inputChangeHandler} 
+                defaultValue={localStorage.getItem('phonenumber')}
+              /><br/>
+
+              <button onClick={this.onUpdateHandler.bind(this)}>SAVE CHANGES</button>
+              <button onClick={this.onCancelHandler.bind(this)}>CANCEL</button>
+            </div>
+          }
+        </div>
       </div>
     );
   }
