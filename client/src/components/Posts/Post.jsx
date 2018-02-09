e61e2aeb97bfd2a89cf9464b713801999d8db535
Jaehyun Chi <jaehchi@gmail.com>
2018-02-08 19:36:23 -0800

    [add] - post add

--- client/src/components/Posts/Post.jsx
+++ client/src/components/Posts/Post.jsx
@@ -7,17 +7,52 @@ class Post extends Component {
   constructor(props) {
     super(props);
   }
+  async onAddHandler () {
+    const payload = {
+      property_id: this.props.currentProperty.id,
+      user_id: this.props.userData.id, 
+      title: document.getElementsByName('title')[0].value.toString(),
+      content: document.getElementsByName('content')[0].value.toString(),
+      date: document.getElementsByName('date')[0].value.toString(),
+      photo_url: document.getElementsByName('photo')[0].value.toString()
+    }
+    const { data } = await axios.post(`http://localhost:3396/api/articles/addArticle`, payload);
+    const d =  await axios.get(`http://localhost:3396/api/articles/fetchAllArticles/${this.props.currentProperty.id}`);
+    this.props.setArticlesData(d.data);
+    data ?  await this.props.setArticleEditState('0') : null;
+  }
+
+  async onAddHandler () {
+    const payload = {
+      username: this.props.,
+      test: ,
+      date: ,
+      article_id,
+      parent_id
+    }
+    console.log('clicked');
+    console.log(this.props.post)
+  }
+  
+  async onUpdateHandler () {
+
+  }
+
+  async onDeleteHandler () {
+
+  }
 
   render() {
     return (
       <div>
         <br/>
-        {/* {JSON.stringify(this.props.post)} */}
+        {JSON.stringify(this.props.post)}
         <li>
           <div className="username">{ this.props.post.username }</div>
           <div className="text">{ this.props.post.text }</div>
           <div className="date">{ this.props.post.date }</div> 
           <ul>
+          <button onClick={this.onAddHandler.bind(this)}>Reply</button>
           { this.props.post.children ?  this.props.post.children.map( child => {
             return ( 
               <div className="">
@@ -26,6 +61,7 @@ class Post extends Component {
               </div>
             )
           }) : null}
+          
           </ul>
         </li>
         <br/>
@@ -33,20 +69,6 @@ class Post extends Component {
     );
   }
 }
-
-/**
-<ul>
-  <li>response1
-  <ul>
-    <li>response1a</li>
-    <li>response1b
-      <ul>
-        <li>response1b1</li>
-      </ul>
-    </li>
-  </ul>
-</ul>
- */
 
 const mapStateToProps = state => {
   return {
